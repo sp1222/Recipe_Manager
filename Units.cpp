dfa359cd6010129398247c968970c3975de0140a
sp1222 <66361833+sp1222@users.noreply.github.com>
2021-02-02 17:09:50 -0600

    Add files via upload
    added overloaded operator functions.  This was discovered while testing Recipe.h.

---
+++ Units.cpp
@@ -0,0 +1,165 @@
+#include "Units.h"
+#include<iostream>
+
+bool Units::operator == (const Units& u) const
+{
+	return unit == u.unit;
+}
+
+bool Units::operator != (const Units& u) const
+{
+
+	return !operator==(u);
+}
+
+// conversion function definitions
+bool doesTypeMatchForConversion(Type from, Type to)
+{
+	if (from == to)
+		return true;	
+	return false;
+}
+
+float standardUnitConversion(Unit start, Unit end, float value)
+{
+	// increment if we are going up in scale, ounce to pounds
+	while (start != end)
+	{
+		if (start < end)
+		{
+			value *= UNIT_CONVERSION_VALUES[int(start)];
+			start = Unit(int(start) + 1);
+		}
+		// decrement if we are going down in scale, pounds to ounce.
+		else if (start > end)
+		{
+			value /= UNIT_CONVERSION_VALUES[int(start) - 1];
+			start = Unit(int(start) - 1);
+		}
+	}
+	return value;
+}
+
+
+// Units class function definitions
+Units::Units(){}
+
+ Units::Units(string& u)
+{
+	// set the unitStr value in all caps.
+	// WILL toupper AND COMMA REMOVAL BE REQUIRED WHEN USING DROPDOWN BOXES?
+	for (auto& s : u)
+	{
+		if (s == ',')
+			s = ' ';
+		s = toupper(s);
+	}
+	unitStr = u;
+
+	// set the unit enum value.
+	for (int i = 0; i < sizeof(STR_UNIT)/sizeof(STR_UNIT[0]); i++)
+	{
+		if (u == STR_UNIT[i])
+		{
+			unit = static_cast<Unit>(i);
+			setType(i);
+			unitStrIndex = i;
+			break;
+		}
+	}	
+}
+
+void Units::setType(int t)
+{
+	switch (t)
+	{
+		// if u is POUND, OUNCEWEIGHT, KILOGRAM, GRAM
+		// then thisType == WEIGHT	
+		case static_cast<int>(Unit::POUND) :
+		case static_cast<int>(Unit::OUNCEWEIGHT) :
+		case static_cast<int>(Unit::KILOGRAM) :
+		case static_cast<int>(Unit::GRAM) :
+			type = Type::WEIGHT;
+			break;
+
+		// if u is GALLON, HALFGALLON, QUART, PINT, CUP OUNCEFLUID, TABLESPOON, TEASPOON, LITER, MILLILITER
+		// then thisType == VOLUME
+		case static_cast<int>(Unit::GALLON) :
+//		case static_cast<int>(Unit::HALFGALLON) :
+		case static_cast<int>(Unit::QUART) :
+		case static_cast<int>(Unit::PINT) :
+		case static_cast<int>(Unit::CUP) :
+		case static_cast<int>(Unit::OUNCEFLUID) :
+		case static_cast<int>(Unit::TABLESPOON) :
+		case static_cast<int>(Unit::TEASPOON) :
+		case static_cast<int>(Unit::LITER) :
+		case static_cast<int>(Unit::MILLILITER) :
+			type = Type::VOLUME;
+			break;
+
+		// if u is EACH
+		// then thisType == COUNT
+		case static_cast<int>(Unit::EACH) :
+		case static_cast<int>(Unit::DOZEN) :
+//		case static_cast<int>(Unit::STICK) :
+			type = Type::COUNT;
+			break;
+
+		// else
+		// thisType == NONE
+		default:
+			type = Type::NONE;
+			break;
+	}
+	// set the string of StrType to 
+	typeStr = STR_TYPE[static_cast<int>(type)];
+	typeStrIndex = static_cast<int>(type);
+}
+
+void Units::setUnit(string& u)
+{
+	// set the unitStr value in all caps.
+	// WILL toupper AND COMMA REMOVAL BE REQUIRED WHEN USING DROPDOWN BOXES?
+	for (auto& s : u)
+	{
+		if (s == ',')
+			s = ' ';
+		s = toupper(s);
+	}
+	unitStr = u;
+
+	// set the unit enum value.
+	for (int i = 0; i < sizeof(STR_UNIT) / sizeof(STR_UNIT[0]); i++)
+	{
+		if (u == STR_UNIT[i])
+		{
+			unit = static_cast<Unit>(i);
+			setType(i);
+			unitStrIndex = i;
+			break;
+		}
+	}
+}
+
+string Units::getUnitStr()
+{
+	return unitStr;
+}
+
+
+Unit Units::getUnit()
+{
+	return unit;
+}
+
+
+string Units::getTypeStr()
+{
+	return typeStr;
+}
+
+
+Type Units::getType()
+{
+	return type;
+}
---
+++ Units.h
@@ -0,0 +1,107 @@
+#pragma once
+#ifndef UNITS_H
+#define UNITS_H
+#include<string>
+using namespace std;
+// question, what if we want to add measurements to this?
+
+// constants for enums Unit and Type
+const string STR_UNIT[] = { "NONE", "GRAM", "OUNCEWEIGHT", "POUND", "KILOGRAM",
+                                            "MILLILITER", "TEASPOON", "TABLESPOON", "OUNCEFLUID",
+                                            "CUP","PINT", "QUART", "LITER", "GALLON",
+                                            "EACH", "DOZEN" };//, "STICK"};
+const string STR_TYPE[] = { "NONE", "WEIGHT", "VOLUME", "COUNT", };
+
+// enumerators that define the Unit and Unit Type.
+enum class Unit
+{
+    NONE,
+    GRAM,
+    OUNCEWEIGHT,
+    POUND,
+    KILOGRAM,
+    MILLILITER,
+    TEASPOON,
+    TABLESPOON,
+    OUNCEFLUID,
+    CUP,
+    PINT,
+    QUART,
+    LITER,
+    GALLON,
+    EACH,
+    DOZEN,
+//    STICK
+};
+
+enum class Type
+{
+    NONE,
+    WEIGHT,
+    VOLUME,
+    COUNT
+};
+
+// Units class for instances of units.
+class Units
+{
+private:	
+    Unit unit = Unit::NONE;
+    Type type = Type::NONE;
+    int unitStrIndex = 0;
+    int typeStrIndex = 0;
+    string unitStr = STR_UNIT[unitStrIndex];
+    string typeStr = STR_TYPE[typeStrIndex];
+    
+    // private functions
+    void setType(int t);
+
+public:
+    bool operator == (const Units& u) const;
+    bool operator != (const Units& u) const;
+    Units();
+    Units(string& unit);
+    void setUnit(string& unit);
+    string getUnitStr();
+    Unit getUnit();
+    string getTypeStr();
+    Type getType();
+
+};
+
+// prototype functions for conversions
+bool doesTypeMatchForConversion(Type from, Type to);
+float standardUnitConversion(Unit start, Unit end, float value);
+
+/*
+// constants for unit conversions
+//weights
+const float GRAM_TO_OUNCEWEIGHT = 0.0353;
+const float OUNCEWEIGHT_TO_POUND = 0.0625;
+const float POUND_TO_KILOGRAM = 0.4536;
+
+// volumes
+const float MILLILITER_TO_TEASPOON = 0.2029;
+const float TEASPOON_TO_TABLESPOON = 0.3333;
+const float TABLESPOON_TO_OUNCEFLUID = 0.5;
+const float OUNCEFLUID_TO_CUP = 0.125;
+const float CUP_TO_PINT = 0.5;
+const float PINT_TO_QUART = 0.5;
+const float QUART_TO_GALLON = 0.25;
+
+// counts
+const float EACH_TO_DOZEN = .0833;
+*/
+
+// index zero represents converting from NONE to something
+// in this case, NONE is unconvertible, so we give it a value of -1.
+// this includes mismatch type conversions, such as from KILOGRAM to MILLILITER
+
+// to convert incrementally, match the start Unit enum with its respective index value in UNIT_CONVERSION_VALUES
+// ounce to pounds, get index 2, which is OUNCEWEIGHT's enum, and MULTIPLY the value to be converted by the value at index
+// to convert decrementally, match the start Unit enum - 1 with its respective index value.
+// pounds to ounce, get index 3 - 1, which is POUND's enum - 1, and DIVIDE the value to be converted by the value at index
+const float UNIT_CONVERSION_VALUES[] = { -1, 0.035274, 0.0625, 0.453592, -1, 0.202884, 0.3333333, 0.5, 0.125, 0.5, 0.5, 0.9463534, 0.264172, -1, 0.0833 };
+
+
+#endif
