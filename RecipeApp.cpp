fe235b9743bb80e6edc49da5429cdb7edb5555e5
sp1222 <66361833+sp1222@users.noreply.github.com>
2021-02-25 10:05:31 -0600

    Add files via upload
    Start of implementing GUI to the Recipe Manager.  Far from complete, simply backing up to github for the time being.

---
+++ RecipeApp.cpp
@@ -0,0 +1,256 @@
+#include "RecipeApp.h"
+
+//*************************************************************************************************
+// RecipeApp definition
+
+wxIMPLEMENT_APP(RecipeApp);
+bool RecipeApp::OnInit()
+{
+    MainFrame* Main = new MainFrame(_("Recipe Manager App"));
+    Main->Show(true);
+    SetTopWindow(Main);
+    return true;
+}
+
+
+//*************************************************************************************************
+// ListCtrl definition
+
+
+//*************************************************************************************************
+// ListCtrl function definitions
+
+void ListCtrl::InsertItemInRecipeListReportDisplay(Recipe& r, int& i)
+{
+    wxString str;
+    str = wxString(r.getName());
+    long temp = InsertItem(i, str);
+    SetItemData(temp, i);
+
+    str = wxString(r.getMealType());
+    SetItem(temp, 1, str);
+}
+
+//*************************************************************************************************
+// MainFrame Event Table
+
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+    EVT_MENU(ABOUT, MainFrame::OnAbout)
+    EVT_MENU(EXIT, MainFrame::OnExit)
+
+    EVT_MENU(RECIPE_LIST_REPORT_DISPLAY, MainFrame::OnRecipeListReportDisplay)
+    EVT_MENU(INGREDIENT_LIST_DISPLAY, MainFrame::OnIngredientListDisplay)
+    EVT_MENU(CATEGORY_LIST_DISPLAY, MainFrame::OnCategoryListDisplay)
+
+//    EVT_MENU(LIST_TOGGLE_BELL, MainFrame::OnToggleBell)
+//    EVT_MENU(LIST_ROW_LINES, MainFrame::OnSetRowLines)
+
+
+
+//    EVT_UPDATE_UI(LIST_ROW_LINES, MainFrame::OnUpdateRowLines)
+wxEND_EVENT_TABLE()
+
+//*************************************************************************************************
+// MainFrame definition
+
+MainFrame::MainFrame(const wxString& title): wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 500))
+{
+    // load lists.
+    loadCategoryList(catFile, categories);
+    loadIngredientList(ingredFile, ingredients, categories);
+    loadRecipeList(recipeFile, recipes, ingredients, mealTypes);
+
+    // Menu Bar
+    // File menu
+    wxMenu* menuFile = new wxMenu;
+    menuFile->Append(ABOUT, "&About\tAlt-A");
+    menuFile->AppendSeparator();
+    menuFile->Append(EXIT, "&Quit\tAlt-Q");
+
+    // List menu
+    wxMenu* menuList = new wxMenu;
+    menuList->Append(RECIPE_LIST_REPORT_DISPLAY, "Display &Recipe List\tAlt-R");
+    menuList->Append(INGREDIENT_LIST_DISPLAY, "Display &Ingredient List\tAlt-I");
+    menuList->Append(CATEGORY_LIST_DISPLAY, "Display &Category List\tAlt-C");
+    menuList->AppendSeparator();
+
+    // Define menu bar
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuList, "&Lists");
+    SetMenuBar(menuBar);
+
+    panel = new wxPanel(this, wxID_ANY);
+
+    RebuildList(RECIPE_LIST_REPORT_DISPLAY, wxLC_REPORT);
+
+    wxBoxSizer* const sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(listController, wxSizerFlags(2).Expand().Border());
+    panel->SetSizer(sizer);
+
+    SetClientSize(panel->GetBestSize());
+}
+
+
+//*************************************************************************************************
+// MainFrame function definitions
+
+void MainFrame::OnAbout(wxCommandEvent& WXUNUSED(e))
+{
+    wxMessageDialog dialog(this, "Recipe Manager System\nStephen Polozoff (c) 2021", "About Recipe Manager");
+    dialog.ShowModal();
+}
+
+void MainFrame::OnExit(wxCommandEvent& WXUNUSED(e))
+{
+    Close(TRUE);
+}
+
+void MainFrame::OnRecipeListReportDisplay(wxCommandEvent& WXUNUSED(e))
+{
+    // here we clear the current listController and display the recipe list.
+    RebuildList(RECIPE_LIST_REPORT_DISPLAY, wxLC_REPORT);
+}
+
+void MainFrame::OnIngredientListDisplay(wxCommandEvent& e)
+{
+    // here we clear the current listController and display the ingredient list.
+}
+
+void MainFrame::OnCategoryListDisplay(wxCommandEvent& e)
+{
+    // here we clear the current listController and display the category list.
+}
+
+void MainFrame::RebuildList(long custFlags, long wxFlags, bool withText)
+{
+#if 0
+    if (!listController || ((flags & wxLC_VIRTUAL) 1 = (listController->GetWindowStyleFlag() & wxLC_VIRTUAL)))
+#endif
+    {
+        wxListCtrl* const old = listController;
+        listController = new ListCtrl(panel, LIST_CTRL, wxDefaultPosition, wxDefaultSize, wxFlags | wxBORDER_THEME | wxLC_SINGLE_SEL);
+
+        if (old)
+        {
+            wxSizer* const sizer = panel->GetSizer();
+            sizer->Replace(old, listController);
+            delete old;
+            sizer->Layout();
+        }
+
+        switch (wxFlags & wxLC_MASK_TYPE)
+        {
+            case wxLC_REPORT:
+                switch (custFlags)
+                {
+                    case RECIPE_LIST_REPORT_DISPLAY:
+                        BuildRecipeListReportDisplay();
+                        break;
+                    default:
+                        wxFAIL_MSG("Unknown Display Request");
+                        break;
+                }
+                break;
+            default:
+                wxFAIL_MSG("Unknown wxLC_STYLE Request");
+                break;
+        }
+//        wxMenuBar* const mb = GetMenuBar();
+//        if (mb)
+//            listController->EnableBellOnNoMatch(mb->IsChecked(LIST_TOGGLE_BELL));
+    }
+//    GetMenuBar()->Check(LIST_ROW_LINES, false);
+}
+
+void MainFrame::BuildRecipeListReportDisplay()
+{
+    wxListItem col;
+    col.SetText("Recipe Name");
+    col.SetImage(-1);
+    listController->InsertColumn(0, col);
+
+    col.SetText("Meal Type");
+    listController->InsertColumn(1, col);
+
+    // servings, keywords, etc..
+
+    listController->Hide();                 // speeds up the processes of adding items to the list.
+    
+    // populate the list with the recipe information.
+    int i = -1;
+    for (auto& recipe : recipes)
+    {
+        i++;
+        listController->InsertItemInRecipeListReportDisplay(recipe, i);
+        
+    }
+    listController->Show();
+
+    listController->SetColumnWidth(0, wxLIST_AUTOSIZE);
+    listController->SetColumnWidth(1, wxLIST_AUTOSIZE);
+}
+
+/*
+void MainFrame::BuildIngredientListDisplay()
+{
+    wxListItem col;
+    col.SetText("Ingredient Name");
+    col.SetImage(-1);
+    listController->InsertColumn(0, col);
+
+    col.SetText("Ingredient Type");
+    listController->InsertColumn(1, col);
+
+    listController->Hide();                 // speeds up the processes of adding items to the list.
+
+    wxStopWatch sw;
+
+    // populate the list with the ingredient information.
+//    for(int i = 0; i < )
+
+
+    listController->Show();
+}
+
+void MainFrame::BuildCategoryListDisplay()
+{
+    wxListItem col;
+    col.SetText("Category");
+    col.SetImage(-1);
+    listController->InsertColumn(0, col);
+
+    col.SetText("Ingredient Count");
+    listController->InsertColumn(1, col);
+
+    listController->Hide();                 // speeds up the processes of adding items to the list.
+
+    wxStopWatch sw;
+
+    // populate the list with the category information.
+//    for(int i = 0; i < )
+
+
+    listController->Show();
+}*/
+
+/*
+* 
+* Having issues with this debugging..
+* 
+void MainFrame::OnToggleBell(wxCommandEvent& e)
+{
+    listController->EnableBellOnNoMatch(e.IsChecked());
+}
+
+void MainFrame::OnSetRowLines(wxCommandEvent& e)
+{
+    listController->EnableAlternateRowColours(e.IsChecked());
+    listController->Refresh();
+}
+
+void MainFrame::OnUpdateRowLines(wxUpdateUIEvent& e)
+{
+    e.Enable(listController->HasFlag(wxLC_VIRTUAL));
+}
+*/
---
+++ RecipeApp.h
@@ -0,0 +1,113 @@
+#pragma once
+#ifndef RECIPEAPP_H
+#define RECIPEAPP_H
+
+#include "RecipeList.h"
+#include<wx/listctrl.h>
+#include<wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include<wx/wx.h>
+#endif
+    
+
+//*************************************************************************************************
+// RecipeApp
+
+class RecipeApp: public wxApp
+{
+public:
+    virtual bool OnInit();
+private:
+};
+
+
+//*************************************************************************************************
+// ListCtrl
+
+class ListCtrl : public wxListCtrl
+{
+public:
+    ListCtrl(wxWindow* parent, const wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
+        :wxListCtrl(parent, id, pos, size, style)
+    {
+        updated = -1;
+    }
+
+    void InsertItemInRecipeListReportDisplay(Recipe& r, int& i);
+private:
+    long updated;
+};
+
+
+//*************************************************************************************************
+// MainFrame
+
+class MainFrame: public wxFrame
+{
+public:
+    MainFrame(const wxString& title);
+
+protected:
+    // file drop down menu
+    void OnAbout(wxCommandEvent& e);
+    void OnExit(wxCommandEvent& e);
+
+    void OnRecipeListReportDisplay(wxCommandEvent& e);
+    void OnIngredientListDisplay(wxCommandEvent& e);
+    void OnCategoryListDisplay(wxCommandEvent& e);
+
+//    void OnToggleBell(wxCommandEvent& e);
+//    void OnSetRowLines(wxCommandEvent& e);
+
+
+//    void OnUpdateRowLines(wxUpdateUIEvent& e);
+
+    wxPanel* panel;
+    // MyListCtrl pointer object
+    ListCtrl* listController;
+
+private:
+    // do these objects go here?
+    list<Category> categories;
+    list<Ingredient> ingredients;
+    list<pair<string, int>> mealTypes;
+    list<Recipe> recipes;
+    string catFile = "testCategoryFile.csv";
+    string ingredFile = "testIngredientFile.csv";
+    string recipeFile = "testRecipeFile.csv";
+
+    // functions that redefine the list controller object.
+    void RebuildList(long custFlags, long wxFlags = wxLC_REPORT, bool withText = true);
+    void BuildRecipeListReportDisplay();
+    void BuildIngredientListDisplay();
+    void BuildCategoryListDisplay();
+
+    wxDECLARE_NO_COPY_CLASS(MainFrame);
+    wxDECLARE_EVENT_TABLE();
+};
+
+wxDECLARE_APP(RecipeApp);
+
+//*************************************************************************************************
+// IDs for menu commands.
+
+enum
+{
+    ABOUT = wxID_ABOUT,
+    EXIT = wxID_EXIT,
+
+    RECIPE_LIST_REPORT_DISPLAY = wxID_HIGHEST + 1,
+    INGREDIENT_LIST_DISPLAY,
+    CATEGORY_LIST_DISPLAY,
+
+
+    LIST_TOGGLE_BELL,
+    LIST_ROW_LINES,
+
+
+
+    LIST_CTRL
+
+};
+
+#endif // !RECIPEAPP_H
